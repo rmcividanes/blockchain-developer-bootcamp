5e2baf3e091e3bb0fc05834acbed2963075f7e93
rmcividanes <rmcividanes1994@gmail.com>
2023-05-09 19:38:28 -0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || "";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    localhost: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(","),
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(","),
+    },
+  },
+};
---
+++ src/components/App.js
@@ -0,0 +1,95 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import config from "../config.json";
+
+import {
+  loadProvider,
+  loadNetwork,
+  loadAccount,
+  loadTokens,
+  loadExchange,
+  subscribeToEvents,
+  loadAllOrders,
+} from "../store/interactions";
+
+import Navbar from "./Navbar";
+import Markets from "./Markets";
+import Balance from "./Balance";
+import Order from "./Order";
+import PriceChart from "./PriceChart";
+import Transactions from "./Transactions";
+import Trades from "./Trades";
+import OrderBook from "./OrderBook";
+import Alert from "./Alert";
+
+function App() {
+  const dispatch = useDispatch();
+
+  const loadBlockchainData = async () => {
+    //Connect Ethers to blockchain
+    const provider = await loadProvider(dispatch);
+
+    //Fetch cuurent network's chainID (e.g. hardhat)
+    const chainId = await loadNetwork(provider, dispatch);
+
+    //Reload page when network changes
+    window.ethereum.on("chainChanged", () => {
+      window.location.reload();
+    });
+
+    //Fetch current account & balance from Metamask when changed
+    window.ethereum.on("accountsChanged", () => {
+      loadAccount(provider, dispatch);
+    });
+
+    //Load Token Smart Contract
+    const DApp = config[chainId].DApp;
+    const mETH = config[chainId].mETH;
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch);
+
+    //Load exchange smart contract
+    const exchangeConfig = config[chainId].exchange;
+    const exchange = await loadExchange(
+      provider,
+      exchangeConfig.address,
+      dispatch
+    );
+
+    //Fetch all orders: open, filled, cancelled
+    loadAllOrders(provider, exchange, dispatch);
+
+    //Listen to events
+    subscribeToEvents(exchange, dispatch);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  });
+  return (
+    <div>
+      <Navbar />
+
+      <main className="exchange grid">
+        <section className="exchange__section--left grid">
+          <Markets />
+
+          <Balance />
+
+          <Order />
+        </section>
+        <section className="exchange__section--right grid">
+          <PriceChart />
+
+          <Transactions />
+
+          <Trades />
+
+          <OrderBook />
+        </section>
+      </main>
+      <Alert />
+    </div>
+  );
+}
+
+export default App;
